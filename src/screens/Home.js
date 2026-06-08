848328adc0df736420533b8ba00a5eb1d22280a7
mpenaday <mpenaday@udesa.edu.ar>
2026-06-08 20:37:38 -0300

    hice profile

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,7 @@ import { auth, db } from "../firebase/config";
 import { FlatList } from "react-native-web";
 import Comentarios from "../components/Comentarios";
 import Post from "../components/Post";
+import Profile from "./Profile";
 
 function Home(props) {
     const [posteo, setPosteo] = useState([]);
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,49 @@
 import { Text } from "react-native";
 import { View, Pressable } from "react-native-web";
+import { auth, db} from "../firebase/config";
+import Post from "../components/Post";
+import { FlatList } from "react-native";
+import { useState, useEffect } from "react";
 
 function Profile(props) {
+    const [posteo, setPosteo] = useState([]);
+    
+        const [loading, setLoading] = useState(true);
+    
+        useEffect(() => {
+    
+            db.collection("Posts").onSnapshot(docs => {
+    
+                let posteo = [];
+    
+                docs.forEach(doc => {
+    
+                    posteo.push({
+    
+                        id: doc.id,
+    
+                        data: doc.data()
+    
+                    });
+    
+                });
+    
+                setPosteo(posteo);
+                console.log(posteo)
+    
+                setLoading(false);
+    
+            });
+    
+        }, []);
     return (
         <View>
+            <text>{auth.currentUser.email}</text>
+            <FlatList
+                data={posteo}
+                keyExtractor={item => item.id.toString()}
+                renderItem={({item}) => <Post id={item.id} data= {item.data}/>}
+            />
             
             <Pressable
                 onPress={() => props.navigation.navigate('Login')}>
