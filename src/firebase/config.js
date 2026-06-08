c6bbdcdb486d0dde21756524dfdd3152a83c3b43
Yannick-sys <ymayo@udesa.edu.ar>
2026-06-08 19:55:52 -0300

    Inicializacion de repo

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import app from 'firebase/app';
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDC9IzWB1xp7zpzcFHhZHfW2t23pBuUhv0",
+  authDomain: "pi-wainbarg-mayo-pena3-public.firebaseapp.com",
+  projectId: "pi-wainbarg-mayo-pena3-public",
+  storageBucket: "pi-wainbarg-mayo-pena3-public.firebasestorage.app",
+  messagingSenderId: "37706936481",
+  appId: "1:37706936481:web:97ba02f3627cdf617380e0"
+};
+
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = app.firestore();
